392824434f2c4180a279c6036274e76a7f6a4ef3
rushiv026 <rushivyavahare2002@gmail.com>
2025-03-23 12:23:28 +0530

    first BE changes

---
+++ backend/backend/src/main/java/com/backend/controller/Mainrouting.java
@@ -0,0 +1,17 @@
+package com.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ResponseBody
+public class Mainrouting {
+
+    @GetMapping("/start")
+    public String start()
+    {
+        return "Hello";
+    }
+
+}
